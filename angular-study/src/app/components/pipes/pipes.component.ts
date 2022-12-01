e79d59685f6159211d9bab8771b2daf7499f0dd9
josue052015 <76499684+josue052015@users.noreply.github.com>
2022-11-30 23:23:00 -0400

    pipes finished

--- angular-study/src/app/components/pipes/pipes.component.ts
+++ angular-study/src/app/components/pipes/pipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-pipes',
@@ -11,6 +12,8 @@ export class PipesComponent  {
   amount = 10000.3756
   percent = 0.50
 
+//i18n pipes show something based on the value in the map (the value to compare comes from the property that we applied the pipe)
+
   //i18n select
   personNumbers = '1'
 
@@ -18,5 +21,26 @@ export class PipesComponent  {
     '1': 'invitarlo',
     '2': 'invitarlos'
   }
+
+  //i18n plural 
+
+  // unlike i18n select, i18n plural select the value based on conditions. So, you can have a dynamic result
+
+  //If we change the information here, the message will display based on the quantity of pillars
+  pillars = ['tomioka', 'rengoku','zanemi']
+
+  pillarsMap = {
+    '=0': 'there´s no pillar here',
+    '=1': 'We have one pillar here',
+    'other': 'We have # pillars here'
+  }
+
+  objectExample = {
+    name:'Pedro',
+    lastName: 'Rodriguez'
+  }
+
+  //interval is a rxjs method that creates an Observable that emits sequential numbers every specified interval of time
+  counterObservable = interval(1000)
 
 }
